8f36f0c6efd99b8ee26478ea09220ea5d81ecee6
miguelzackk <mzsilvaw@gmail.com>
2025-11-27 09:18:36 -0300

    fix: fix to run in railway

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
 
-$host = "aws-1-us-east-2.pooler.supabase.com";
+echo "<h1>Servidor PHP rodando no Railway! ✔️</h1>";
+
+$host = "db.ecbgnduxbpgxyajevdgz.supabase.co";
 $port = "5432";
 $dbname = "postgres";
-$user = "postgres.ecbgnduxbpgxyajevdgz";
+$user = "postgres";
 $password = "Fifa2@Migue";
 
 try {
     $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conectou!";
+
+    echo "<p>Conectou ao Supabase com sucesso! ✔️</p>";
+
 } catch (Exception $e) {
-    echo "Erro: " . $e->getMessage();
+    echo "<p>Erro ao conectar: " . $e->getMessage() . "</p>";
 }
