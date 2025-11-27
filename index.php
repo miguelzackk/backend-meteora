68c0ed7710da3ef831d57a858c8c1f9304af0bc2
miguelzackk <mzsilvaw@gmail.com>
2025-11-27 10:12:57 -0300

    fix: install composer

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
     exit();
 }
 
-require __DIR__ . "/../vendor/autoload.php";
+require __DIR__ . "/vendor/autoload.php";
 
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
